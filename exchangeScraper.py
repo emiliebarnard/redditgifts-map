c8eeb6f0c89be64fcf8321645349aaa15b76b3fd
embarnard <emiliebarnard@gmail.com>
2014-04-08 21:04:15 -0700

    share link loading logo and location of pins correctly

--- exchangeScraper.py
+++ exchangeScraper.py
@@ -26,24 +26,24 @@ soup3 = BeautifulSoup(urllib2.urlopen('http://redditgifts.com/exchanges/?page=3'
 ##############################################################################
 ##############################################################################
 
-# exchangeLinks1 = soup1.findAll("a", {"class": "exchangelink"})
-# exchangeLinks1 = exchangeLinks1[::-1] #reverses the list so most recent is last
-# 
-# exchangeLinks2 = soup2.findAll("a", {"class": "exchangelink"})
-# exchangeLinks2 = exchangeLinks2[::-1]
-# 
-# exchangeLinks3 = soup3.findAll("a", {"class": "exchangelink"})
-# exchangeLinks3 = exchangeLinks3[::-1]
-# 
-# allExchangeLinks = exchangeLinks3 + exchangeLinks2 + exchangeLinks1
-# 
-# exchangeNames = [] #this will store the name of all exchanges
-# 
-# for linkElement in allExchangeLinks:
-# 	# elementExchangeName = str(linkElement).replace("</a>", "")
-# 	# for 
-# 	elementExchangeName = str(linkElement.getText())
-# 	exchangeNames.append(elementExchangeName)
+exchangeLinks1 = soup1.findAll("a", {"class": "exchangelink"})
+exchangeLinks1 = exchangeLinks1[::-1] #reverses the list so most recent is last
+
+exchangeLinks2 = soup2.findAll("a", {"class": "exchangelink"})
+exchangeLinks2 = exchangeLinks2[::-1]
+
+exchangeLinks3 = soup3.findAll("a", {"class": "exchangelink"})
+exchangeLinks3 = exchangeLinks3[::-1]
+
+allExchangeLinks = exchangeLinks3 + exchangeLinks2 + exchangeLinks1
+
+exchangeNames = [] #this will store the name of all exchanges
+
+for linkElement in allExchangeLinks:
+	# elementExchangeName = str(linkElement).replace("</a>", "")
+	# for 
+	elementExchangeName = str(linkElement.getText())
+	exchangeNames.append(elementExchangeName)
 
 
 ##############################################################################
@@ -60,41 +60,43 @@ soup3 = BeautifulSoup(urllib2.urlopen('http://redditgifts.com/exchanges/?page=3'
 ################################PART TWO#######################################
 #####this will print the code needed to numerical exchange lookup table in js
 
-# name = ""
-# # print exchangeNames 
-# for index, name in enumerate(exchangeNames[:-1]): #loops through all but last element in list
-# # 	print '"' + name+ '": function(){'
-# # 	print '\treturn '+ str(index) + ';'
-# # 	print'},'
-# # print '"' + exchangeNames[-1] + '": function(){'
-# # print '\treturn' + str(index+1) + ';'
-# # print'}'
-# 	print '"' + name+ '": ' + str(index) + ','
-# print '"' + exchangeNames[-1] + '": ' + str(index+1)
+####for share link table, add lowercase condition
+
+name = ""
+# print exchangeNames 
+for index, name in enumerate(exchangeNames[:-1]): #loops through all but last element in list
+# 	print '"' + name+ '": function(){'
+# 	print '\treturn '+ str(index) + ';'
+# 	print'},'
+# print '"' + exchangeNames[-1] + '": function(){'
+# print '\treturn' + str(index+1) + ';'
+# print'}'
+	print '"' + name.lower()+ '": ' + str(index) + ','
+print '"' + exchangeNames[-1].lower() + '": ' + str(index+1)
 
 ##############################################################################
 ###############################PART THREE#####################################
 
-exchangeImages1 = soup1.find_all(src=re.compile("exchange-logo"))
-exchangeImages1 = exchangeImages1[::-1] #reverses the list so most recent is last
-
-exchangeImages2 = soup2.find_all(src=re.compile("exchange-logo"))
-exchangeImages2 = exchangeImages2[::-1] #reverses the list so most recent is last
-
-exchangeImages3 = soup3.find_all(src=re.compile("exchange-logo"))
-exchangeImages3 = exchangeImages3[::-1] #reverses the list so most recent is last
-
-allImages = exchangeImages3 + exchangeImages2 + exchangeImages1
-
-allImageURLs = []
-
-for imageURL in allImages:
-	allImageURLs.append(imageURL["src"])
-	# str(imageURL).replace('<img src="',"")
-	# str(imageURL).replace('" width="100"/>',"")
-
-#####this will print the code needed lookup the image url by exchange number
-
-for index, imageURL in enumerate(allImageURLs[:-1]): #loops through all but last element in list
-	print '"' + str(index) + '": "' + imageURL + '",'
-print '"' + str(index+1) + '": "' + allImageURLs[-1] +'"'
+# exchangeImages1 = soup1.find_all(src=re.compile("exchange-logo"))
+# exchangeImages1 = exchangeImages1[::-1] #reverses the list so most recent is last
+# 
+# exchangeImages2 = soup2.find_all(src=re.compile("exchange-logo"))
+# exchangeImages2 = exchangeImages2[::-1] #reverses the list so most recent is last
+# 
+# exchangeImages3 = soup3.find_all(src=re.compile("exchange-logo"))
+# exchangeImages3 = exchangeImages3[::-1] #reverses the list so most recent is last
+# 
+# allImages = exchangeImages3 + exchangeImages2 + exchangeImages1
+# 
+# allImageURLs = []
+# 
+# for imageURL in allImages:
+# 	allImageURLs.append(imageURL["src"])
+# 	# str(imageURL).replace('<img src="',"")
+# 	# str(imageURL).replace('" width="100"/>',"")
+# 
+# #####this will print the code needed lookup the image url by exchange number
+# 
+# for index, imageURL in enumerate(allImageURLs[:-1]): #loops through all but last element in list
+# 	print '"' + str(index) + '": "' + imageURL + '",'
+# print '"' + str(index+1) + '": "' + allImageURLs[-1] +'"'
